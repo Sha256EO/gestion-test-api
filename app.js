0cb1834cc5fe3ac7a2efc2b163d7473b6a3dfdc1
Sha256EO <sha256evakorunn@outlook.com>
2024-12-05 18:29:05 -0300

    se corrigen algunos errores

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }
 
